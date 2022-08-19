bf2af781e08c9d283ac60465d3870ec15d444d43
hoytp3381 <111385590+hoytp3381@users.noreply.github.com>
2022-08-19 11:42:59 -0700

    Unit 1
    StarsAndStripes Assignment

---
+++ Unit 01/StarsAndStripes.java
@@ -0,0 +1,38 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Paul Hoyt
+//Date - 8/19/22
+//Class - APCSA
+//Lab  - Stars and Stripes aplus
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+   }
+
+   public void printTwentyDashes()
+   {
+   }
+
+   public void printTwoBlankLines()
+   {
+   }
+   
+   public void printASmallBox()
+   {	
+   }
+ 
+   public void printABigBox()
+   { 	
+   }   
+}
