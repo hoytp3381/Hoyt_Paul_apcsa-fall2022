fc7fb215933385449320e743e748b394b69bc4ec
hoytp3381 <111385590+hoytp3381@users.noreply.github.com>
2022-08-19 10:51:34 -0700

    Unit 0
    All 4 assignments under one folder

---
+++ Unit 00/src/AsciiBox.java
@@ -0,0 +1,24 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Paul Hoyt
+//Date - 8/18/22
+//Class - APCSA
+//Lab  - AsciiBox 0a 1
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Paul \t  8/18/22 \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+
+	}
+}
