68dc10c4aa8d563619a67234f721d1907fd73275
hoytp3381 <111385590+hoytp3381@users.noreply.github.com>
2022-08-22 11:03:52 -0700

    Unit 1
    Added under a Folder

---
+++ Unit1-InstructionExamples/StarsRunner.java
@@ -0,0 +1,27 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+//      StarsAndStripes ref = new StarsAndStripes();
+//      //call the methods needed to make the patterns on the word document
+//      ref.printASmallBox();
+//      ref.printTwoBlankLines();
+//      ref.printABigBox();
+//      ref.printTwoBlankLines();
+//      
+//      ref.UnitedStatesFlag();
+	   System.out.print(7%3);
+      
+   }
+}
