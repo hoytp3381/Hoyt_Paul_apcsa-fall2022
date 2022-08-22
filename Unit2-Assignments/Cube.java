b37bf5f0f39e3c36e04102a4797aa2cc6601d189
hoytp3381 <111385590+hoytp3381@users.noreply.github.com>
2022-08-22 11:26:47 -0700

    Unit 2
    Initial Upload

---
+++ Unit2-Assignments/Cube.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name - Paul Hoyt
+//Date - 8/22/22
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		return (side*side)*6;
+	}
+}
---
+++ Unit2-Assignments/Trap.java
@@ -0,0 +1,13 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name - Paul Hoyt
+//Date - 8/22/22
+//Class - APCSA
+//Lab  - TrapArea Lab
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		return ((base1+base2)*height)/2;
+	}
+}
